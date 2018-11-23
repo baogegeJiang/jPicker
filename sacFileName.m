1f2c315b3a365b3a3b8e8aae7c3f35603d7c44f0
baogegeJiang <2320140745@qq.com>
2018-11-23 12:41:42 +0800

    readme

--- sacFileName.m
+++ sacFileName.m
@@ -1,4 +1,8 @@
-function[ sacE ,sacN, sacZ]=sacFileName(net,station,comp,sDay0);
+function[ sacE ,sacN, sacZ]=sacFileName(net,station,comp,sDay0)
+%sacE={'ABC.20150101.00.BHE.SAC','ABC.20150101.08.BHE.SAC','ABC.20150101.16.BHE.SAC'}
+%sacN={'ABC.20150101.00.BHN.SAC','ABC.20150101.08.BHN.SAC','ABC.20150101.16.BHN.SAC'}
+%sacZ={'ABC.20150101.00.BHZ.SAC','ABC.20150101.08.BHZ.SAC','ABC.20150101.16.BHZ.SAC'}
+%comp: BH/HH/...
 setPara;
 setPath;
 sacDir=dataDir;
@@ -10,37 +14,16 @@ sDay=sDay0-yNum+1;
 dayS=sprintf('%03d',sDay);
 clear temp dir
 YM=YMD(1:6);
-if strcmp(net,'NM') || strcmp(net,'GS')
-   sacDirLst=sprintf('/home/jiangyr/sacDir/%s.%s/%s/',net,station,YM);
-   fileLst=dir(sprintf('%s/%s.%s.%s.%sE.SAC',sacDirLst,net,station,YMD,comp));
-   if length(fileLst)==0;sacE=cell(1,1);sacE{1}='';sacN=sacE;sacZ=sacE;return;end
-   for i=1:length(fileLst)
-       sacE{i}=[sacDirLst,fileLst(i).name];
-   end
-
- fileLst=dir(sprintf('%s/%s.%s.%s.%sN.SAC',sacDirLst,net,station,YMD,comp));
-   if length(fileLst)==0;sacE=cell(1,1);sacE{1}='';sacN=sacE;sacZ=sacE;return;end
-   for i=1:length(fileLst)
-       sacN{i}=[sacDirLst,fileLst(i).name];
-   end
-
-  fileLst=dir(sprintf('%s/%s.%s.%s.%sZ.SAC',sacDirLst,net,station,YMD,comp));
-   if length(fileLst)==0;sacE=cell(1,1);sacE{1}='';sacN=sacE;sacZ=sacE;return;end
-   for i=1:length(fileLst)
-       sacZ{i}=[sacDirLst,fileLst(i).name];
-   end
-return
-end
 sacDirL=length(sacDir);
 sacDirLst={'/media/geodyn/1410090015032623/','/media/geodyn/1508282016033123/','/media/geodyn/1503270015082820/'};
 for i=1:24
-for j=1:3
-hourSacDir=[sacDirLst{j},'/',YMD,'.',dayS,'.',sprintf('%02d',i-1),'0000.000/'];
-temp=dir([hourSacDir,net,'.',station,'.',Y,dayS,'*.',comp,'E*']);
-sacE{i+(j-1)*24}=[hourSacDir(sacDirL+1:end),temp.name];
-temp=dir([hourSacDir,net,'.',station,'.',Y,dayS,'*.',comp,'N*']);
-sacN{i+(j-1)*24}=[hourSacDir(sacDirL+1:end),temp.name];
-temp=dir([hourSacDir,net,'.',station,'.',Y,dayS,'*.',comp,'Z*']);
-sacZ{i+(j-1)*24}=[hourSacDir(sacDirL+1:end),temp.name];
-end
+    for j=1:3
+        hourSacDir=[sacDirLst{j},'/',YMD,'.',dayS,'.',sprintf('%02d',i-1),'0000.000/'];
+        temp=dir([hourSacDir,net,'.',station,'.',Y,dayS,'*.',comp,'E*']);
+        sacE{i+(j-1)*24}=[hourSacDir(sacDirL+1:end),temp.name];
+        temp=dir([hourSacDir,net,'.',station,'.',Y,dayS,'*.',comp,'N*']);
+        sacN{i+(j-1)*24}=[hourSacDir(sacDirL+1:end),temp.name];
+        temp=dir([hourSacDir,net,'.',station,'.',Y,dayS,'*.',comp,'Z*']);
+        sacZ{i+(j-1)*24}=[hourSacDir(sacDirL+1:end),temp.name];
+    end
 end
